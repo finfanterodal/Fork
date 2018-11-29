ed5502d1ca56685cb49bfd0f48197020f01a4ba0
finfanterodal <finfanterodal@nautilus11.danielcastelao.org>
2018-11-29 08:58:23 +0100

    Commit A Master

---
+++ src/fork/Fork.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fork;
+
+/**
+ *
+ * @author finfanterodal
+ */
+public class Fork {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+    //ESTAMOS HACIENDO UN FORK
+    //HOLA
+    /*    
+    */
+    }
+    
+}
